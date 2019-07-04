cd3043a1f3db0844e3baa5e76edda8392d3edc42
TalhaIkhlaq <talhaikhlaq@gmail.com>
2019-07-04 17:22:26 +0100

    webpack and packagejson setup

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const { dbURI, port } = require('./config/environment')
 
+
+
+
+app.listen(port, () => console.log(`App is listening on port ${port}`))
