d0aada0c7926718af20c353581c22107290ca2ab
AmyWilson <amydaniellewilson@gmail.com>
2019-07-04 17:37:21 +0100

    port working

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ const mongoose = require('mongoose')
 const app = express()
 const { dbURI, port } = require('./config/environment')
 
-
-
-
+console.log('I am rocking')
 app.listen(port, () => console.log(`App is listening on port ${port}`))
