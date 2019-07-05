c0b0b95a57545418c06d03f71305ad47fb1ed14a
AmyWilson <amydaniellewilson@gmail.com>
2019-07-05 15:13:03 +0100

    profile seeds added

--- db/seeds.js
+++ db/seeds.js
@@ -1,38 +1,77 @@
 const mongoose = require('mongoose')
 const { dbURI } = require('../config/environment')
-const User = require('../models/user')
+const Human = require('../models/humanProfile')
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, db) => {
-  if (err) return console.log(err)
+  if (err) console.log(err)
   db.dropDatabase()
-    .then(() => {
-      return User.create([
+    // .then(() => {
+    //   return User.create([
+    //
+    //   ])
+    // })
+    .then(users => {
+      console.log(`${users.length} users created`)
+      return Human.create([
         {
-          username: 'tal',
-          email: 'tal@email',
-          password: 'pass',
-          passwordConfirmation: 'pass'
-        }, {
-          username: 'amy',
-          email: 'amy@email',
-          password: 'pass',
-          passwordConfirmation: 'pass'
-        }, {
-          username: 'kasia',
-          email: 'kasia@email',
-          password: 'pass',
-          passwordConfirmation: 'pass'
-        }, {
-          username: 'wesleydale',
-          email: 'wes@email',
-          password: 'pass',
-          passwordConfirmation: 'pass'
+          ownerName: 'Steven Parker',
+          ownerAge: 28,
+          ownerOccupation: 'Lawyer',
+          pet: [{
+            petName: 'Charming Nuttypants',
+            petAge: 2,
+            petSpecies: 'Cat',
+            hobbies: ['Running, ', 'Drinking water straight from the tap'],
+            aboutMe: 'Prefers the term collector rather than hoarder. Likes batting fragile things off tables to hear them smash and currently at war with neighborhood pigeons. ',
+            image: 'https://i.imgur.com/Io6f0Eo.jpg?1'
+          }]
+          // user: users[0]
+        },
+        {
+          ownerName: 'Ruby Charlton',
+          ownerAge: 35,
+          ownerOccupation: '3D Animator',
+          pet: [{
+            petName: 'Poofy Fluff',
+            petAge: 8,
+            petSpecies: 'Cat',
+            hobbies: ['Jangling Keys Manically '],
+            aboutMe: 'Loves bright colors and soft fabrics. Using the litter box is a continuous struggle that is continuously up for debate.',
+            image: 'https://i.imgur.com/w0wFWaQ.jpg?1'
+          }]
+          // user: users[0]
+        },
+        {
+          ownerName: 'Rian Brantle',
+          ownerAge: 30,
+          ownerOccupation: 'Producer',
+          pet: [{
+            petName: 'Dancing Glitterbug',
+            petAge: 4,
+            petSpecies: 'Dog',
+            hobbies: ['Bounding about, ', 'Giving Kisses'],
+            aboutMe: 'Greedy eater and stubborn. I was stubborn to house train. Unable to look straight ahead. ',
+            image: 'https://i.imgur.com/hyS04HX.jpg?1'
+          }]
+          // user: users[0]
+        },
+        {
+          ownerName: 'Victoria Lancaster',
+          ownerAge: 42,
+          ownerOccupation: 'Application Analyst',
+          pet: [{
+            petName: 'Rainy Gooberton',
+            petAge: 12,
+            petSpecies: 'Dog',
+            hobbies: ['Bounding about, ', 'Giving Kisses'],
+            aboutMe: 'Greedy eater and stubborn. I was stubborn to house train. Unable to look straight ahead. ',
+            image: 'https://i.imgur.com/hyS04HX.jpg?1'
+          }]
+          // user: users[0]
         }
       ])
     })
-    .then(users => {
-      console.log(`${users.length} users created`)
-    })
+    .then(profiles => console.log(`${profiles.length} profiles created`))
     .catch(err => console.log(err))
     .finally(() => mongoose.connection.close())
 })
