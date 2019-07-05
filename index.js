6ccaca780404cfd875cc1a2e5f18d59af3dbe6b4
TalhaIkhlaq <talhaikhlaq@gmail.com>
2019-07-05 10:44:45 +0100

    index.js packages required

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
 const { dbURI, port } = require('./config/environment')
+const router = require('./config/router')
+const logger = require('./lib/logger')
+const errorHandler = require('./lib/errorHandler')
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true })
+
+app.use(bodyParser.json())
+
+app.use(logger)
+
+app.use('/api', router)
+
+app.use(errorHandler)
 
 console.log('Backend is rocking')
 app.listen(port, () => console.log(`App is listening on port ${port}`))
