080c7e95e2e5bdf46aef9bccd822fbdcb8e73229
KasiaW <kasia.aguti@gmail.com>
2019-07-06 15:37:28 +0100

    controllers

--- config/router.js
+++ config/router.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const users = require('../controllers/auth')
 // const secureRoute = require('../lib/secureRoute')
 const profiles = require('../controllers/usersAndPets')
+const locations  = require('../controllers/locations')
 
 router.route('/register')
   .post(users.register)
@@ -17,5 +18,15 @@ router.route('/profiles/:id')
   .get(profiles.showProfile)
   .put(profiles.editProfile)
   .delete(profiles.deleteProfile)
+
+
+router.route('/locations')
+  .get(locations.indexLocations)
+  .post(locations.createLocation)
+
+router.route('/locations/:id')
+  .get(locations.showLocation)
+  .put(locations.editLocation)
+  .delete(locations.deleteLocation)
 
 module.exports = router
