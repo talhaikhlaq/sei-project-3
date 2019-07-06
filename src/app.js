feefde9beb54ffef086128b6ed19127aea409b87
AmyWilson <amydaniellewilson@gmail.com>
2019-07-06 14:57:13 +0100

    webpack working and front end connected

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>PET SOCIAL BY TAL KASIA & AMY</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
