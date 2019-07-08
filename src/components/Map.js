1248e213166be064261fcd9a6e2a8639cbf98cb3
KasiaW <kasia.aguti@gmail.com>
2019-07-08 12:03:23 +0100

    mapboxgl and env

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+// const express = require('express')
+// const app = express()
+const axios = require('axios')
+
+require('dotenv').config()
+const mapbox = process.env.MAPBOX
+
 class Map extends React.Component {
   render() {
     return (
