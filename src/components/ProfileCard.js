058dfda8cca8a37fcdb92a1e09445279e6ee3d26
AmyWilson <amydaniellewilson@gmail.com>
2019-07-08 18:21:29 +0100

    profiles are indexed - styling needs further work

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const ProfileCard = ({ ownerName, image }) => {
+const ProfileCard = ({ ownerName, image, pet }) => {
   return (
-    <div className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
-      <div className="card">
-        <div className="card-header">
-          <h4 className="card-header-title">{ownerName}</h4>
-        </div>
-        <div className="card-image">
-          <figure className="image">
-            <img src={image} alt={ownerName} />
-          </figure>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-square">
+          <img className="is-rounded" src={image} alt={ownerName}/>
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-128x128">
+              <img src={pet[0].image} alt={pet[0].petName}/>
+            </figure>
+          </div>
+          <div className="media-content">
+            <p className="title is-4">{ownerName}</p>
+            <p className="subtitle is-5">Pet: {pet[0].petName}</p>
+          </div>
         </div>
       </div>
     </div>
