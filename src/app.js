e0e0226ee5d0b444f9ed468efcdf3067c2dd613a
AmyWilson <amydaniellewilson@gmail.com>
2019-07-08 14:06:49 +0100

    removed imports from app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'mdbreact/dist/css/mdb.css'
-import './assets/scss/mdb.scss'
-import './assets/scss/style.scss'
 
 import Home from './components/common/Home'
 import Navbar from './components/common/Navbar'
