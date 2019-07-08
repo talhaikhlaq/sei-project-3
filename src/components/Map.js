100cf5383ec0c7df939b20bba9f2aaf9b019bb99
KasiaW <kasia.aguti@gmail.com>
2019-07-08 12:27:02 +0100

    changes-to-webpack

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import mapboxgl from 'mapbox-gl'
 
 // const express = require('express')
 // const app = express()
 const axios = require('axios')
 
-require('dotenv').config()
-const mapbox = process.env.MAPBOX
+mapboxgl.accessToken = process.env.MAPBOX
+// const mapbox = process.env.MAPBOX
 
 class Map extends React.Component {
-  render() {
-    return (
-      <div>Map</div>
-    )
+render() {
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapDiv,
+      style: 'mapbox://styles/gaebar/cjx3dxidn01941comlfwaysve',
+
+      zoom: 1
+    })
   }
+
+
+}
+
+
+
 }
 
 export default Map
