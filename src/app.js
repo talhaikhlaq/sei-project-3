69be1650797a190c7c1d9a23edcbb9aff26f5663
AmyWilson <amydaniellewilson@gmail.com>
2019-07-08 11:40:21 +0100

    makde home, navbar map files and folders

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom'
-import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+
+import Home from '../common/Home'
+import Navbar from '../common/Navbar'
+import Map from './components/Map'
 
 class App extends React.Component {
   render() {
     return (
-      <div>PET THINGY</div>
+      <BrowserRouter>
+        <main>
+          <Navbar />
+          <Switch>
+            <Route path="/map" component={Map} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </main>
+      </BrowserRouter>
+
     )
   }
 }
---
+++ src/components/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div>Map</div>
+    )
+  }
+}
+
+export default Map
