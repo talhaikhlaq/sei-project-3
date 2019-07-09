772ad5c6d3be987813e85430bd47acb4e9f5e42c
TalhaIkhlaq <talhaikhlaq@gmail.com>
2019-07-09 10:50:14 +0100

    copy pasted correct ProfileShow.js

--- src/components/profile/ProfileShow.js
+++ src/components/profile/ProfileShow.js
@@ -16,9 +16,7 @@ class ProfileShow extends React.Component {
   }
 
   render() {
-
     if (!this.state.profile) return null
-    if (this.state.profile) return null
     const { profile } = this.state
     return (
       <main className="section">
@@ -31,15 +29,6 @@ class ProfileShow extends React.Component {
                 <img src={profile.image} alt={profile.ownerName} />
               </figure>
               <hr />
-          <h2 className="title">{profile.ownerName}</h2>
-          <hr />
-          <div className="columns">
-            <div className="column is-half">
-              <figure className="image">
-                <img src={profile.image} alt={profile.ownerName} />
-              </figure>
-            </div>
-            <div className="column is-half">
               <h4 className="title is-4">Age</h4>
               <p>{profile.ownerAge}</p>
               <hr />
@@ -54,24 +43,8 @@ class ProfileShow extends React.Component {
                 <img src={profile.pet[0].image} alt={profile.pet[0].petName} />
               </figure>
               <hr />
-                <h4 className="title is-4">Species</h4>
-                <p>{profile.pet[0].petSpecies}</p>
-                <hr />
-                <h4 className="title is-4">Hobbies</h4>
-                <p>{profile.pet[0].hobbies}</p>
-                <hr />
-                <h4 className="title is-4">About Me</h4>
-                <p>{profile.pet[0].aboutMe}</p>
-            </div>
-          </div>
-        </div>
-      </main>
-
-              <h4 className="title is-4">Pet</h4>
-              <p>{profile.pronouns}</p>
-              <hr />
-              <h4 className="title is-4">Looking For: </h4>
-              <p>{profile.lookingFor}</p>
+              <h4 className="title is-4">Species</h4>
+              <p>{profile.pet[0].petSpecies}</p>
               <hr />
               <h4 className="title is-4">Hobbies</h4>
               <p>{profile.pet[0].hobbies}</p>
