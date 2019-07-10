e483b259211703f1e2ac1a363bfc383d1fd8567c
KasiaW <kasia.aguti@gmail.com>
2019-07-10 11:58:15 +0100

    basic structure, route and  css for msgs

---
+++ src/components/messages/MessageList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+
+
+
+
+class MessageList extends React.Component {
+  render() {
+    return (
+      <h1>dog</h1>
+
+    )
+  }
+}
+
+
+
+
+export default MessageList
+
+
+//
+// <ul className="message-list">
+//   {this.props.messages.map(message => {
+//     return (
+//       <li key={message.id}>
+//         <div>
+//           {message.senderId}
+//         </div>
+//         <div>
+//           {message.text}
+//         </div>
+//       </li>
+//     )
+//   })}
+// </ul>
