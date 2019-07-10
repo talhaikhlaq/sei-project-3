eb0793df3850f3d6e9acdc13aa728c2a7432c8af
AmyWilson <amydaniellewilson@gmail.com>
2019-07-10 16:02:53 +0100

    new profile show page

--- src/components/profile/ProfileShowTrial.js
+++ src/components/profile/ProfileShowTrial.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Auth from '../../lib/Auth'
@@ -26,36 +26,42 @@ class ProfileShowTrial extends React.Component {
     return (
       <main className="main-show">
         <div className="profile-show">
-          <div className="profile-heading">
-            <h2 id="names">{profile.ownerName} & {profile.pet[0].petName}</h2>
-          </div>
-
-          <div className="profile-body">
-            <p>About {profile.ownerName} <br/> Age: {profile.ownerAge} <br/> Occupation: {profile.ownerOccupation}</p>
-
-            <p>About {profile.pet[0].petName} <br/>
-            Age: {profile.pet[0].petAge} <br/>
-            Species: {profile.pet[0].petSpecies} <br/>
-            Hobbies: {profile.pet[0].hobbies} <br/>
-            About Me: {profile.pet[0].aboutMe}
-            </p>
+          <div className="profile-text">
+            <div className="profile-heading">
+              <h2 id="names">{profile.ownerName} & {profile.pet[0].petName}</h2>
+            </div>
+            <div className="profile-body">
+              <p className="about-p">About {profile.ownerName} <br/> Age: {profile.ownerAge} <br/> Occupation: {profile.ownerOccupation}</p>
+              <p className="about-p">About {profile.pet[0].petName} <br/>
+              Age: {profile.pet[0].petAge} <br/>
+              Species: {profile.pet[0].petSpecies} <br/>
+              Hobbies: {profile.pet[0].hobbies} <br/>
+              About Me: {profile.pet[0].aboutMe}
+              </p>
+            </div>
+            {this.isOwner() && <Link
+              className="button edit-button"
+              to={`/friends/edit/${profile._id}`}
+            >
+            Edit Profile
+            </Link>}
           </div>
 
           <div className="profile-pictures">
-          <figure>
-          <img src={profile.image} alt={profile.ownerName} />
-          </figure>
-            <figure>
-              <img src={profile.pet[0].image} alt={profile.pet[0].petName} />
-            </figure>
+            <div
+              className="profile-picture"
+              style={{
+                backgroundImage: `url(${profile.image})`
+              }}
+            />
+            <div
+              className="profile-picture"
+              style={{
+                backgroundImage: `url(${profile.pet[0].image})`
+              }}
+            />
           </div>
 
-          {this.isOwner() && <Link
-            className="button edit-button"
-            to={`/friends/edit/${profile._id}`}
-          >
-            Edit Profile
-          </Link>}
         </div>
 
       </main>
