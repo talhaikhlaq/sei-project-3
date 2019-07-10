07950efe800eb890772300fdb24149a63d041400
KasiaW <kasia.aguti@gmail.com>
2019-07-10 17:08:49 +0100

    chat app

--- src/components/messages/MessageApp.js
+++ src/components/messages/MessageApp.js
@@ -1,42 +1,80 @@
 import React from 'react'
-import MessageList from './MessageList'
+import Auth from '../../lib/Auth'
 
+import MessageList from './MessageList'
+import SendMessageForm from './SendMessageForm'
+import Chatkit from '@pusher/chatkit-client'
+
+const instanceLocator = 'v1:us1:8a39e96d-aa83-484a-b425-a7abc05dfa51'
+
+const testToken = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/8a39e96d-aa83-484a-b425-a7abc05dfa51/token'
+
+const userId = Auth.getPayload().sub
+
+const roomId = '24849940'
 
 
 
 class MessageApp extends React.Component {
-
   constructor() {
     super()
     this.state = {
-      messages: DUMMY_DATA
+      messages: []
     }
+    this.sendMessage = this.sendMessage.bind(this)
+  }
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: instanceLocator,
+      userId: userId,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: testToken
+      })
+    })
+
+    chatManager.connect()
+      .then(currentUser => {
+        console.log('doing something 1')
+        this.currentUser = currentUser
+        this.currentUser.subscribeToRoom({
+          roomId: roomId,
+          hooks: {
+            onMessage: message => {
+              console.log('doing something 2')
+              this.setState({
+                messages: [...this.state.messages, message]
+              })
+            }
+          }
+        })
+      })
+  }
+
+  sendMessage(text) {
+    this.currentUser.sendMessage({
+      text,
+      roomId: roomId
+    })
   }
 
   render() {
     return (
       <div className="app">
-        <MessageList />
-
+        <Title />
+        <MessageList
+          roomId={this.state.roomId}
+          messages={this.state.messages} />
+        <SendMessageForm
+          sendMessage={this.sendMessage} />
       </div>
     )
   }
 }
 
 
-
-
-const DUMMY_DATA = [
-  {
-    senderId: 'perborgen',
-    text: 'who\'ll win?'
-  },
-  {
-    senderId: 'janedoe',
-    text: 'who\'ll win?'
-  }
-]
-
-
+function Title() {
+  return <p className="msgtitle">My awesome chat app</p>
+}
 
 export default MessageApp
