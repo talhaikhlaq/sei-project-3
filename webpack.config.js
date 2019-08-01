c0d745ce9a46572faa049b380107aa6062863427
AmyWilson <amydaniellewilson@gmail.com>
2019-08-01 13:34:52 +0100

    deploying attempt

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const Dotenv = require('dotenv-webpack')
+// const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: './src/app.js',
